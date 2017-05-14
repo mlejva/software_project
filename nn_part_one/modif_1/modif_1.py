2ae1930a5b41fc64f4a87fec236b99ddbbc64dfb
mlejva <vasek.mlejnsky@gmail.com>
2017-05-14 23:38:12 +0200

    Updates

--- nn_part_one/modif_1/modif_1.py
+++ nn_part_one/modif_1/modif_1.py
@@ -1,148 +1,165 @@
 import os
 import sys
-sys.path.append('../') # Because video_preprocessor is in the parent folder
-import cv2
-import video_preprocessor as video_prep
 import shutil
-import math
 import numpy as np
 from network_modif_1 import Network
+sys.path.append('../')  # Because video_preprocessor is in the parent folder
+import video_preprocessor as video_prep
 
-def make_epoch_dir(name, epoch):
-    epoch_dir_name = "./saved_frames/%s/epoch-%d" % (name, epoch)
+def make_epoch_dir(name, current_epoch):
+    """Creates a directory for the current epoch and returns its name"""
 
-    if os.path.exists(epoch_dir_name):
-        shutil.rmtree(epoch_dir_name)
-    os.makedirs(epoch_dir_name)
+    dir_name = "./saved_frames/%s/epoch-%d" % (name, current_epoch)
 
-    return epoch_dir_name
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+    os.makedirs(dir_name)
 
-def target_pixel_accuracy(network_predictions, gold):    
-    right_predictions = 0    
+    return dir_name
+
+def target_pixel_accuracy(network_predictions, gold):
+    """Computes a ratio of right predictions"""
+
+    right_predictions = 0
 
     batch_size = np.size(gold, 0)
     for frame_index in range(batch_size):
         predicted_frame = network_predictions[frame_index]
         predicted_frame *= 255
-        np.clip(predicted_frame, 0, 255)
 
         gold_frame = gold[frame_index]
         gold_frame *= 255
 
-        predicted_target_pixel_position = np.where(predicted_frame == predicted_frame.min())
+        #predicted_target_pixel_position = np.where(predicted_frame == predicted_frame.min())
         gold_target_pixel_position = np.where(gold_frame == gold_frame.min())
-        
+
         if int(predicted_frame[gold_target_pixel_position]) == int(predicted_frame.min()):
-            right_predictions += 1        
+            right_predictions += 1
 
     accuracy = right_predictions / batch_size
     return accuracy
 
 def background_distance(network_predictions, gold):
-    # Computes average L2 distance of gold and predicted frames in a batch
-    
+    """Computes average L2 distance of gold and predicted frames in a batch"""
     average_distance = 0
+
     batch_size = np.size(gold, 0)
     for frame_index in range(batch_size):
         predicted_frame = network_predictions[frame_index]
-        #predicted_frame *= 255
         np.clip(predicted_frame, 0, 1)
 
         gold_frame = gold[frame_index]
-        #gold_frame *= 255
 
         average_distance += np.linalg.norm(gold_frame.astype(int) - predicted_frame.astype(int))
-
     average_distance /= batch_size
     return average_distance
-def perform_train(network, input, gold_output, epoch):        
-    loss, _ = network.train(input_frames, gold_output_frames, True, network.training_step == 0)                    
-    print("\tLoss: %f" % loss)     
 
-def perform_validation(network, input, gold_output, epoch):
-    print("Running validation (epoch: %d)..." % (epoch + 1))
+def perform_train(network, network_input, network_gold_output):
+    """Runs training on a network and prints loss"""
 
-    loss, predictions = network.evaluate("validation", input, gold_output, True)    
-    print("\tLoss on validation: %f" % loss)    
+    loss, _ = network.train(network_input,
+                            network_gold_output,
+                            True,
+                            network.training_step == 0)
+    print("\tLoss: %f" % loss)
+
+def perform_validation(network, network_input, network_gold_output, current_epoch):
+    """Evaluate a validation input on a network and prints loss and accuracy"""
+
+    print("Running validation (epoch: %d)..." % (current_epoch + 1))
+
+    loss, predictions = network.evaluate("validation", network_input, network_gold_output, True)
+    print("\tLoss on validation: %f" % loss)
 
     # Compute accuracy and background distance
-    target_accuracy = target_pixel_accuracy(predictions, gold_output)
-    network.log_scalar_test(target_accuracy, "target_pixel_accuracy", epoch, "validation")
+    target_accuracy = target_pixel_accuracy(predictions, network_gold_output)
+    network.log_scalar_test(target_accuracy, "target_pixel_accuracy", current_epoch, "validation")
     print("\tTarget pixel accuracy on validation: %.2f" % (target_accuracy * 100.0))
 
-    average_distance = background_distance(predictions, gold_output)
-    network.log_scalar_test(average_distance, "background_distance_average", epoch, "validation")
+    average_distance = background_distance(predictions, network_gold_output)
+    network.log_scalar_test(average_distance,
+                            "background_distance_average",
+                            current_epoch,
+                            "validation")
     print("\tAverage distance on validation: %.2f" % average_distance)
 
-def perform_test(network, input, gold_output, base, epoch):
-    print("Running testing (epoch: %d)..." % (epoch + 1))
+def perform_test(network, network_input, network_gold_output, base, current_epoch):
+    """Evaluate a test input on a network and prints loss and accuracy.
+    Saves network predictions"""
 
-    loss, predictions = network.evaluate("test", input, gold_output, True)        
+    print("Running testing (epoch: %d)..." % (current_epoch + 1))
+
+    loss, predictions = network.evaluate("test", network_input, network_gold_output, True)
     print("\tLoss on test: %f" % loss)
 
     # Save predictions
-    test_prediction_name_path = base + "/test-prediction-frames-%d" % epoch
+    test_prediction_name_path = base + "/test-prediction-frames-%d" % current_epoch
     video_prep.save_prediction_frames(test_prediction_name_path, predictions, version="1.1")
 
     # Compute accuracy and background distance
-    target_accuracy = target_pixel_accuracy(predictions, gold_output)
-    network.log_scalar_test(target_accuracy, "target_pixel_accuracy", epoch, "test")
+    target_accuracy = target_pixel_accuracy(predictions, network_gold_output)
+    network.log_scalar_test(target_accuracy, "target_pixel_accuracy", current_epoch, "test")
     print("\tTarget pixel accuracy on test: %.2f" % (target_accuracy * 100.0))
 
-    average_distance = background_distance(predictions, gold_output)
-    network.log_scalar_test(average_distance, "background_distance_average", epoch, "test")
+    average_distance = background_distance(predictions, network_gold_output)
+    network.log_scalar_test(average_distance, "background_distance_average", current_epoch, "test")
     print("\tAverage distance on test: %.2f" % average_distance)
-    
-    print("================") 
+
+    print("================")
 
 
-def save_test_frames(input, gold_output, base, epoch):
-    '''input_name_path = base + "/test-input-frames-%d" % epoch            
-    video_prep.save_frames(input_name_path, input)'''
+def save_test_frames(test_input, test_gold_output, base, current_epoch):
+    """Saves input and gold output from testing dataset"""
 
-    gold_output_name_path = base + "/test-gold-output-frames-%d" % epoch
-    video_prep.save_frames(gold_output_name_path, gold_output)
+    '''input_name_path = base + "/test-input-frames-%d" % epoch
+    video_prep.save_frames(input_name_path, test_input)'''
 
-
+    gold_output_name_path = base + "/test-gold-output-frames-%d" % current_epoch
+    video_prep.save_frames(gold_output_name_path, test_gold_output)
 
 
 if __name__ == "__main__":
-    batch_size = 7
-    epochs = 200
+    BATCH_SIZE = 7
+    EPOCHS = 400
 
-    vp = video_prep.VideoPreprocessor("./videos", ".mp4", batch_size)
-    vp.prepare_datasets(0.6, 0.2, 0.2)
-    (height, width) = vp.get_training_frame_shape()
-        
-    exp_name = "model-1"
-    network = Network(height, width, exp_name)
-    network.construct()
+    VP = video_prep.VideoPreprocessor("./videos", ".mp4", BATCH_SIZE)
+    VP.prepare_datasets(0.6, 0.2, 0.2)
+    HEIGHT, WIDTH = VP.get_training_frame_shape()
 
-    for epoch in range(epochs):
-        epoch_dir_name = make_epoch_dir(exp_name, epoch)
+    EXP_NAME = "model-1"
+    NETWORK = Network(HEIGHT, WIDTH, EXP_NAME)
+    NETWORK.construct()
+
+    for epoch in range(EPOCHS):
+        epoch_dir_name = make_epoch_dir(EXP_NAME, epoch)
 
         # Train
-        vp.shuffle_training_data()        
+        VP.shuffle_training_data()
         print("Training network (epoch: %d)..." % (epoch + 1))
-        while epoch == vp.epochs_completed:              
-            input_frames, gold_output_frames = vp.next_batch(grayscale=True)                                                
-
-            input_frames, _, _ = video_prep.normalize(input_frames)            
+        while epoch == VP.epochs_completed:
+            input_frames, gold_output_frames = VP.next_batch(grayscale=True)
+            # Preprocess
+            input_frames, _, _ = video_prep.normalize(input_frames)
             gold_output_frames, _, _ = video_prep.normalize(gold_output_frames)
-            perform_train(network, input_frames, gold_output_frames, epoch)
+
+            perform_train(NETWORK, input_frames, gold_output_frames)
+        ####################
 
         # Validation
-        input_frames_val, gold_output_frames_val = vp.validation_dataset(grayscale=True)            
-
+        input_frames_val, gold_output_frames_val = VP.validation_dataset(grayscale=True)
+        # Preprocess
         input_frames_val, _, _ = video_prep.normalize(input_frames_val)
         gold_output_frames_val, _, _ = video_prep.normalize(gold_output_frames_val)
-        perform_validation(network, input_frames_val, gold_output_frames_val, epoch)
 
+        perform_validation(NETWORK, input_frames_val, gold_output_frames_val, epoch)
+        ####################
 
         # Test
-        input_frames_test, gold_output_frames_test = vp.testing_dataset(grayscale=True)
+        input_frames_test, gold_output_frames_test = VP.testing_dataset(grayscale=True)
         save_test_frames(input_frames_test, gold_output_frames_test, epoch_dir_name, epoch)
-
+        # Preprocess
         input_frames_test, _, _ = video_prep.normalize(input_frames_test)
         gold_output_frames_test, _, _ = video_prep.normalize(gold_output_frames_test)
-        perform_test(network, input_frames_test, gold_output_frames_test, epoch_dir_name, epoch)
+
+        perform_test(NETWORK, input_frames_test, gold_output_frames_test, epoch_dir_name, epoch)
+        ####################
--- nn_part_one/modif_1/network_modif_1.py
+++ nn_part_one/modif_1/network_modif_1.py
@@ -1,53 +1,58 @@
 import tensorflow as tf
 import numpy as np
 
-# Classes 
+# Classes
 class Network:
     def __init__(self, frame_height, frame_width, exp_name):
         self.frame_height = frame_height
         self.frame_width = frame_width
         graph = tf.Graph()
-        self.session = tf.Session(graph=graph, config=tf.ConfigProto(intra_op_parallelism_threads=4))       
-    
-        self.train_writer = tf.summary.FileWriter("./logs/network_modif_1/train/%s" % exp_name, self.session.graph)
+        self.session = tf.Session(graph=graph)
+
+        self.train_writer = tf.summary.FileWriter("./logs/network_modif_1/train/%s" % exp_name,
+                                                  self.session.graph)
         self.test_writer = tf.summary.FileWriter("./logs/network_modif_1/test/%s" % exp_name)
 
-    def construct(self):        
+    def construct(self):
         with self.session.graph.as_default():
-            # Network graph
-            self.input_frames = tf.placeholder(tf.float32, [None, self.frame_height, self.frame_width, 2])
-            self.__print_tensor_shape(self.input_frames, "\tinput_frames")
+            # Network input & gold output
+            self.input_frames = tf.placeholder(tf.float32,
+                                               [None, self.frame_height, self.frame_width, 2])
+            self.gold_frames = tf.placeholder(tf.float32,
+                                              [None, self.frame_height, self.frame_width, 1])
 
-            self.gold_output_frames = tf.placeholder(tf.float32, [None, self.frame_height, self.frame_width, 1])
-            self.__print_tensor_shape(self.gold_output_frames, "\tgold_output_frames")
-
-            conv_layer1 = tf.layers.conv2d(self.gold_output_frames, 48, [1, 3], 1, padding="SAME")
+            # Hidden layers
+            conv_layer1 = tf.layers.conv2d(self.input_frames, 24, [1, 3], 1, padding="SAME")
             self.__print_tensor_shape(conv_layer1, "\tconv_layer1")
 
-            conv_layer2 = tf.layers.conv2d(conv_layer1, 48, [3, 1], 1, padding="SAME")
+            conv_layer2 = tf.layers.conv2d(conv_layer1, 24, [3, 1], 1, padding="SAME")
             self.__print_tensor_shape(conv_layer2, "\tconv_layer2")
 
             conv_layer3 = tf.layers.conv2d(conv_layer2, 1, [1, 1], 1, padding="SAME")
             self.__print_tensor_shape(conv_layer3, "\tconv_layer3")
 
             output_layer = conv_layer3
-            self.loss = tf.losses.sigmoid_cross_entropy(self.gold_output_frames, output_layer)            
+
+            gold = tf.sigmoid(self.gold_frames)
+            self.loss = tf.losses.sigmoid_cross_entropy(gold, output_layer)
+            #self.loss = tf.losses.mean_pairwise_squared_error(gold, output_layer)
+
             self.predictions = tf.sigmoid(output_layer)
 
             self.global_step = tf.Variable(0, dtype=tf.int64, trainable=False)
             self.train_step = tf.train.AdamOptimizer().minimize(self.loss, global_step=self.global_step)
             #######
 
-            # Summaries            
+            # Summaries
             self.summaries = {"training": tf.summary.merge([tf.summary.scalar("train/loss", self.loss)])}
-            
+
             for dataset in ["validation", "test"]:
                 self.summaries[dataset] = tf.summary.merge([tf.summary.scalar(dataset + "/loss", self.loss)])
 
             init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
             self.session.run(init)
 
-            self.session.graph.finalize()           
+            self.session.graph.finalize()
             self.train_writer.add_graph(self.session.graph)
 
     # Properties
@@ -60,16 +65,16 @@ class Network:
         print("%s: %s" % (tensor_name, str(tensor.get_shape())))
 
     # Public methods
-    def log_scalar_test(self, value, name, epoch, dataset):                
+    def log_scalar_test(self, value, name, epoch, dataset):
         summary = tf.Summary(value=[
             tf.Summary.Value(tag=dataset + "/" + name, simple_value=value)
         ])
-        self.test_writer.add_summary(summary, epoch)          
+        self.test_writer.add_summary(summary, epoch)
 
     def train(self, input_frames, output_frames, run_summaries=False, run_metadata=False):
         targets = [self.train_step, self.loss, self.predictions]
-        args = {"feed_dict": {self.input_frames: input_frames, self.gold_output_frames: output_frames}}
-        
+        args = {"feed_dict": {self.input_frames: input_frames, self.gold_frames: output_frames}}
+
         if run_summaries:
             targets.append(self.summaries["training"])
         if run_metadata:
@@ -77,9 +82,9 @@ class Network:
             args["run_metadata"] = tf.RunMetadata()
 
         results = self.session.run(targets, **args)
-        
+
         if run_summaries:
-            summary = results[-1]            
+            summary = results[-1]
             self.train_writer.add_summary(summary, self.training_step - 1)
         if run_metadata:
             self.train_writer.add_run_metadata(args["run_metadata"], "step%d" % (self.training_step - 1))
@@ -89,7 +94,7 @@ class Network:
         return loss, predictions
 
     def evaluate(self, dataset, input_frames, output_frames, run_summaries=False):
-         args = {"feed_dict": {self.input_frames: input_frames, self.gold_output_frames: output_frames}}
+         args = {"feed_dict": {self.input_frames: input_frames, self.gold_frames: output_frames}}
          targets = [self.predictions, self.loss]
 
          if run_summaries:
@@ -98,9 +103,9 @@ class Network:
          results = self.session.run(targets, **args)
 
          if run_summaries:
-            summary = results[-1]            
+            summary = results[-1]
             self.test_writer.add_summary(summary, self.training_step)
-         
+
          predictions = results[0]
          loss = results[1]
          return loss, predictions
